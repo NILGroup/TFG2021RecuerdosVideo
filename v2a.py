740022e097dfc9f0c01e5b0411d3bd17e2ba4a7f
Daniel <daalca01@ucm.es>
2021-03-29 19:44:02 +0200

    Primer fujo secuencial de la parte de transcribir

---
+++ v2a.py
@@ -0,0 +1,10 @@
+from moviepy.editor import *
+
+
+
+def v2a(nombre):
+
+    print("--- Proceso video2audio ---")
+    video = VideoFileClip(nombre + ".mp4")
+    audio = video.audio
+    audio.write_audiofile(nombre + ".wav")
