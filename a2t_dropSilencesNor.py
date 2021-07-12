eea761c8103f82e92f0ed16b3ebd94e581ac3580
Daniel <daalca01@ucm.es>
2021-07-12 21:10:18 +0200

    Comprime Chunks y Transcribe

---
+++ a2t_dropSilencesNor.py
@@ -0,0 +1,86 @@
+# importing libraries
+import os
+import shutil
+from datetime import datetime
+from datetime import timedelta
+import speech_recognition as sr
+from pydub import AudioSegment, effects, scipy_effects
+from pydub.silence import split_on_silence
+
+
+formato = "%H:%M:%S"
+
+
+def transcribe(fichero = "GH010191.wav"):
+    
+    print('--- Proceso Separar por Silencios ---')
+
+    msl = 400
+    st = -40
+    ks = 300
+
+    # Fichero donde se almacenará la transcripción
+    filename = f"./filesTranscriptions/({st},{msl})transcrip_ComChunck_" + fichero.split(".")[0] + ".txt"
+    fh = open(filename, "w+", encoding = "ISO-8859-1")
+
+    fh.write(f'\n \t min_silence_len = {msl}, silence_thresh = {st}, keep_silence = {ks}, ComChunck \n\n')
+
+    audio = AudioSegment.from_wav("./audios/" + fichero)
+    chunks, not_silence_ranges = split_on_silence(audio, min_silence_len = msl,
+                                                  silence_thresh = st,
+                                                  keep_silence = ks)
+
+    # Directorio donde se almacena los audios troceados
+    try:
+        if os.path.exists('audio_troceado_ComChunck'):
+            shutil.rmtree('audio_troceado_ComChunck')
+        
+        os.mkdir('audio_troceado_ComChunck')
+        os.chdir('audio_troceado_ComChunck')
+    
+    except(FileExistsError):
+        pass
+
+    print('\33[32m' + datetime.now().strftime(formato) + ' START MAIN' + '\033[0m')
+    print('--- Proceso Transcribir ---')
+    
+    i = 1
+    # Transcripcion de audios troceados
+    for chunk in chunks:
+    
+        # Procesamiento de audios troceados
+        chunk_silent = AudioSegment.silent(600)
+        chunk = effects.compress_dynamic_range(chunk, threshold = -12.0, ratio = 5.0, attack = 1, release = 10.0)
+        chunk = scipy_effects.band_pass_filter(chunk, 200, 3100)
+        chunk = effects.normalize(chunk)
+        audio_chunk = chunk_silent + chunk + chunk_silent
+        audio_chunk.export("./parte{0}.wav".format(i), bitrate = '320k', format = "wav")
+        filename = 'parte' + str(i) + '.wav'
+
+        print("Procesando parte " + str(i) + ".  Duracion = {:.2f}".format(chunk.duration_seconds) +
+              " segundos. Inicio: " + str(timedelta(seconds = not_silence_ranges[i - 1][0] / 1000)) +
+              "  | |  Final: " + str(timedelta(seconds = not_silence_ranges[i - 1][1] / 1000)))
+
+        r = sr.Recognizer()
+        file = filename
+        
+        # recognize the chunk
+        with sr.AudioFile(file) as source:
+            audio_listened = r.record(source)
+        
+        try:
+            rec = r.recognize_google(audio_listened, language = "es-ES")
+            #fh.write("Parte " + str(i))
+            #fh.write(" (" + str(timedelta(seconds = not_silence_ranges[i - 1][0] / 1000)) + ") ")
+            fh.write(rec[0].upper() + rec[1:] + ". " + '\n')
+        except sr.UnknownValueError:
+            print('\33[33m' + 'Could not understand audio' + '\033[0m')
+        except sr.RequestError as e:
+            print('\33[31m' + 'Could not request results. check your internet connection' + '\033[0m')
+            
+        i += 1
+    
+    fh.write('\n' + str(i) + " Partes" + '\n')
+    
+    fh.close()
+    os.chdir('..')
