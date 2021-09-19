321720f3bf1a137d5a5f009748a053871f19fe18
Daniel <daalca01@ucm.es>
2021-09-19 19:00:54 +0200

    funcionalidad de correo.

---
+++ services/email.py
@@ -0,0 +1,31 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+import logging
+
+
+def send_email(to, transcript, summary):
+    sender_email = 'tfg2021ResumenVideoRedNeuronal@gmail.com'
+    subject = '[TFG2021: Resumen Video] Resultados'
+    
+    text = (f"Este es el resultado obtenido del video que has subido a nuestro servicio web: \n\n"
+            f"TRANSCRIPCIÓN:\n\n"
+            f"{transcript}\n\n"
+            f"RESUMEN:\n\n"
+            f"{summary}\n")
+    
+    message = MIMEText(text, 'plain')
+    message['From'] = sender_email
+    message['To'] = to
+    message['Subject'] = Header(subject, 'utf-8')
+    
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(sender_email, 'transformers2021')
+        server.sendmail(sender_email, to, message.as_string())
+    except Exception as ex:
+        logging.exception(ex)
+    finally:
+        server.quit()
+    
+    return
